22884900a24b56753896f8b16c891032db742df5
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-11-26 17:55:12 +0545

    Structure for sidebar

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from '@/app/(components)/Navbar';
+import Sidebar from '@/app/(components)/Sidebar';
 
 const DashboardWrapper = ({children}: {children: React.ReactNode}) => {
   return (
     <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
-      {/* Sidebar */}
+      <Sidebar />
       {/* Main Right */}
       <main className={`flex flex-col w-full bg-gray-50 dark:bg-dark-bg md:pl-64`}>
         <Navbar />
