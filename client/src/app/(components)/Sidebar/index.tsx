1d21241deec0a48b9cb1db8f57c34cea81b6fbb9
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-11-28 18:11:02 +0545

    Connected store to dashboard component

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
             </div>
         </div>
         <div className="flex border-y-[1.5px] items-center gap-5 border-gray-200 px-8 py-4 dark:border-gray-700">
-            <Image src="/logo.png" alt="logo" width={40} height={40} />
+            <Image src="/ujj.jpg" alt="logo" width={40} height={40} />
             <div>
                 <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
                     YOUR TEAM
