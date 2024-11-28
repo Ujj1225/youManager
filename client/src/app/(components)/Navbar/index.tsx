f252f370e5669e7ab060ed0d2fe5018f1759f231
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-11-28 18:27:18 +0545

    Added dark mode

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import {Search, Settings} from 'lucide-react';
-import Link from 'next/link';
+import React from "react";
+import { Moon, Search, Settings, Sun } from "lucide-react";
+import Link from "next/link";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode } from "@/state";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isSideBarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
   return (
     <div className="flex items-center justify-between px-4 py-3 dark:bg-black">
       <div className="flex items-center gap-8">
@@ -16,13 +23,30 @@ const Navbar = () => {
         </div>
       </div>
       <div className="flex items-center">
-        <Link href="/settings" className="h-min w-min rounded p-2 hover:bg-gray-200">
-          <Settings className='h-6 w-6 cursor-pointer dark:text-white' />
+        <button
+          onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
+          className={
+            isDarkMode
+              ? `rounded p-2 dark:hover:bg-gray-700`
+              : `rounded p-2 hover:bg-gray-100`
+          }
+        >
+          {isDarkMode? (
+            <Sun className='h-6 w-6 cursor-pointer dark:text-white' />
+          ):
+          (<Moon className="h-6 w-6 cursor-pointer dark:text-black"/>)
+          }
+        </button>
+        <Link
+          href="/settings"
+          className="{isDarkMode? `h-min w-min rounded dark:hover:bg-gray-700` : `h-min w-min rounded hover:bg-gray-100`} h-min w-min rounded p-2 hover:bg-gray-200"
+        >
+          <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
         </Link>
-        <div className='ml-2 mr-5 hidden md:inline-block bg-gray-300 min-h-[2em] w-[0.1em]'></div>
+        <div className="ml-2 mr-5 hidden min-h-[2em] w-[0.1em] bg-gray-300 md:inline-block"></div>
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
