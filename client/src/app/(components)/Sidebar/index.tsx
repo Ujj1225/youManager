e9f7f3cab37c3d096df570e68e4bdfe55f522b38
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-11-28 18:57:23 +0545

    Added sidebar links

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,7 +1,10 @@
 "use client";
-import { LockIcon } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { Home, LockIcon, LucideIcon } from "lucide-react";
 import Image from "next/image";
-import React, { useState } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { use, useState } from "react";
 
 const Sidebar = () => {
   const [showProjects, setShowProjects] = useState(true);
@@ -10,27 +13,69 @@ const Sidebar = () => {
   const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duration-300 z-40
     dark:bg-black overflow-y-auto bg-white w-64`;
   return (
-  <div className={sidebarClassNames}>
-    <div className="flex h-[100%] w-full flex-col justify-start">
+    <div className={sidebarClassNames}>
+      <div className="flex h-[100%] w-full flex-col justify-start">
         <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
-            <div className="text-xl font-bold text-gray-800 dark:text-white">
-                youManager
+          <div className="text-xl font-bold text-gray-800 dark:text-white">
+            youManager
+          </div>
+        </div>
+        <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+          <Image src="/ujj.jpg" alt="logo" width={40} height={40} />
+          <div>
+            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+              YOUR TEAM
+            </h3>
+            <div className="mt-1 flex items-start gap-2">
+              <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-300" />
+              <p className="text-xs text-gray-500">Private</p>
             </div>
+          </div>
         </div>
-        <div className="flex border-y-[1.5px] items-center gap-5 border-gray-200 px-8 py-4 dark:border-gray-700">
-            <Image src="/ujj.jpg" alt="logo" width={40} height={40} />
-            <div>
-                <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
-                    YOUR TEAM
-                </h3>
-                <div className="mt-1 flex items-start gap-2">
-                    <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-300" />
-                    <p className="text-xs text-gray-500">Private</p>
-                </div>
-            </div> 
-        </div>
+        {/* Navbar links */}
+        <nav className="z-10 w-full">
+          <SidebarLink href="/" icon={Home} label="Home" />
+        </nav>
+      </div>
     </div>
-  </div>
+  );
+};
+
+interface SidebarLinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  // isCollapsed: boolean;
+}
+
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  // isCollapsed
+}: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
+  const screenWidth = window.innerWidth;
+  const dispatch = useAppDispatch();
+  const isSideBarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  return (
+    <Link href={href} className="w-full">
+      <div
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:hover:bg-gray-700 ${isActive ? "bg-gray-100 text-white dark:bg-gray-700" : ""}`}
+      >
+        {isActive && (
+          <div className="height-[100%] absolute left-0 top-0 w-1 bg-blue-200"></div>
+        )}
+        <Icon className={`h-6 w-6 text-gray-800 dark:text-gray-100`} />
+        <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+          {label}
+        </span>
+      </div>
+    </Link>
   );
 };
 
